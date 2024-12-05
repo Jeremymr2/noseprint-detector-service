07df6d63f4ead5a1d8fa51189a1397241e22b82d
Jeremy Machare <jeremy.machare@gmail.com>
2024-12-05 03:16:46 -0500

    feat: adding cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from app.api.predict import model
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=["GET"],
+ allow_headers=["*"],
+)
 
 app.include_router(model, prefix='/api/v1/predict')
